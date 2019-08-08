bef35264aeae86ed56a949551eab3ec6e51ce005
Nik Rowell <nik@nikrowell.com>
2019-08-08 08:40:30 -0600

    continue refining nivo Chart example

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -71,39 +71,39 @@ function Chart({data}) {
         legendOffset: 32
       }}
       axisLeft={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: 'food',
-      legendPosition: 'middle',
-      legendOffset: -40
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'food',
+        legendPosition: 'middle',
+        legendOffset: -40
       }}
       labelSkipWidth={12}
       labelSkipHeight={12}
       labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
       legends={[
-      {
-      dataFrom: 'keys',
-      anchor: 'bottom-right',
-      direction: 'column',
-      justify: false,
-      translateX: 120,
-      translateY: 0,
-      itemsSpacing: 2,
-      itemWidth: 100,
-      itemHeight: 20,
-      itemDirection: 'left-to-right',
-      itemOpacity: 0.85,
-      symbolSize: 20,
-      effects: [
-      {
-          on: 'hover',
-          style: {
-              itemOpacity: 1
-          }
-      }
-      ]
-      }
+        {
+        dataFrom: 'keys',
+        anchor: 'bottom-right',
+        direction: 'column',
+        justify: false,
+        translateX: 120,
+        translateY: 0,
+        itemsSpacing: 2,
+        itemWidth: 100,
+        itemHeight: 20,
+        itemDirection: 'left-to-right',
+        itemOpacity: 0.85,
+        symbolSize: 20,
+        effects: [
+        {
+            on: 'hover',
+            style: {
+                itemOpacity: 1
+            }
+        }
+        ]
+        }
       ]}
       animate={true}
       motionStiffness={90}
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,40 +3,6 @@ import { useAuth } from '../context/auth';
 import Button from './Button';
 import Chart from './Chart';
 
-// const data = {
-//   labels: [
-//     '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm'
-//   ],
-//   datasets: [{
-//     name: 'Mikey',
-//     type: 'bar',
-//     values: [25, 40, 30, 35, 8, 52, 17, 4]
-//   }, {
-//     name: 'Molly',
-//     type: 'bar',
-//     values: [23, 50, 10, 15, 18, 32, 27, 14]
-//   }, {
-//     name: 'Johnny',
-//     type: 'line',
-//     values: [15, 5, 4, 13, 3, 13, 20, 20]
-//   }, {
-//     name: 'Jenny',
-//     type: 'line',
-//     values: [10, 12, 15, 16, 15, 29, 4, 2, 3]
-//   }],
-//   yMarkers: [{
-//     label: 'Extra Credit',
-//     value: 55,
-//     options: {labelPos: 'left'}
-//   }],
-//   yRegions: [{
-//     label: 'Average',
-//     start: 10,
-//     end: 30,
-//     options: {labelPos: 'left'}
-//   }],
-// };
-
 const data = [
   {
     "country": "AD",
